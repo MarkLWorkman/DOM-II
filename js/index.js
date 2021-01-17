c54c46d4cf08b0f5e9493647918027f0b375df96
unknown <calanhaga@aim.com>
2021-01-17 17:17:17 -0500

    Inital commit

--- js/index.js
+++ js/index.js
@@ -1,1 +1,10 @@
-// Your code goes here
+const bottomPage = document.querySelector('.footer');
+const funBus = document.querySelector('.logo-heading');
+const navLinks = document.querySelector('.main-navigation');
+const textPara = document.querySelector('.destination p');
+const picture = document.querySelector('.img-content');
+const boatPicture = document.querySelector('.content-destination img');
+
+funBus.addEventListener('click', (event) => {
+    event.target.style.color = 'red';
+});
